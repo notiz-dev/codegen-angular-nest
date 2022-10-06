8e623ebc8d2ec01056022a8816ef57794a32cc3c
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2022-10-06 10:16:14 +0200

    setup swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,6 +5,7 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
@@ -12,6 +13,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  // setup swagger
+  const config = new DocumentBuilder()
+    .setTitle('Angular NestJS Codegen')
+    .setDescription('Base for codegen for Angular app')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
