be468226ffdd4d86c7dbd4b91d51014c97221c15
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2022-10-06 11:14:23 +0200

    enable cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -23,6 +23,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.enableCors();
+
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
