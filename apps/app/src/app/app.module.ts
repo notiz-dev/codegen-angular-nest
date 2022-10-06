0c7daa0cc974a27e4afa4504add16d2b3510a38b
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2022-10-06 10:58:38 +0200

    specify api url

--- apps/app/src/app/app.module.ts
+++ apps/app/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ApiModule } from '../api/api.module';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ApiModule.forRoot({ rootUrl: environment.apiUrl }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
